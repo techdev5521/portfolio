db0b16b431290d31a33bb46db4eaecb8f48d4d1d
Justin Campbell <campbell.justin.tyler@gmail.com>
2021-04-06 01:07:20 -0400

    Set theme colors

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,4 @@
-import { deepPurple, teal } from '@material-ui/core/colors';
+import purple from '@material-ui/core/colors/purple';
 import { createMuiTheme } from '@material-ui/core/styles'
 
 /**
@@ -13,6 +13,12 @@ export default function theme(darkMode = false) {
     return (
         createMuiTheme({
             "palette": {
+                primary: {
+                    main: purple[500],
+                },
+                secondary: {
+                    main: '#f44336',
+                },
                 "type": darkMode ? "dark" : "light",
             },
         })
